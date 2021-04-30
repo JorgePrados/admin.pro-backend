ebcf7fab81b21a409709c6ce47c4cda4ab7596fa
Jorge Prados Palomo <jpradospalomo@contractor.deloitte.es>
2021-04-30 09:50:08 +0200

    First Commit

---
+++ database/config.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+
+    try{
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true, useUnifiedTopology: true,
+            useCreateIndex: true
+        }); 
+
+        console.log('Db Online');
+    } catch(error) {
+        console.log(error);
+        throw new Error('Error de conexión en base de datos');
+    }
+}
+
+
+module.exports = {
+    dbConnection
+}
---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const express = require('express');
+var cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
+const app = express();
+
+app.use(cors())
+//Base de datos
+dbConnection();
+
+// Rutas
+app.get( '/', (req, res) =>{
+    res.json({
+        ok: true,
+        msg: 'Hola Mundo'
+    });
+});
+
+app.listen( process.env.PORT, () => {
+    console.log('servidor corriendo en puerto ' + process.env.PORT);
+});
